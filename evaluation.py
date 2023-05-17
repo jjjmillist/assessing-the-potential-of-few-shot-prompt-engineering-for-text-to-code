79f761977ad828e1d80fdad3a835e39df2fb1068
Jack <->
2023-05-17 16:11:31 +0100

    Clean up Figure 2/3 scripts

--- evaluation.py
+++ evaluation.py
@@ -1,11 +1,33 @@
-from evaluate import load
 from os import listdir
 from pathlib import Path
 from data import contexts, tests
 import pickle
+import numpy as np
+
+def get_monte_carlo_results():
+    root = Path("results/evaluation/random_prefixes")
+    result_matrix = []
+    for sub in listdir(root):
+        prefix_row = []
+        result_matrix.append(prefix_row)
+
+        with open(root / sub / "code.pickle", "rb") as file:
+            _, results = pickle.load(file)
+
+        for problem_index in results.keys():
+            problem_row = []
+            prefix_row.append(problem_row)
+            candidate_results = results[problem_index]
+            for result in candidate_results:
+                candidate_did_pass = result[1]["passed"]
+                problem_row.append(candidate_did_pass)
+        
+    return np.array(result_matrix)
 
 
 if __name__ == "__main__":
+    from evaluate import load
+
     code_eval = load("code_eval")
 
     mbpp_contexts = list(contexts())
--- figure_2.py
+++ figure_2.py
@@ -1,27 +1,16 @@
+from os import listdir
+from pathlib import Path
 import pickle
 
 import numpy as np
 import matplotlib.pyplot as pyplot
 
-with open("results/codeparrot-mbpp-28-04-23@20:50:35.pickle", "rb") as file:
-    data = pickle.load(file)
+from evaluation import get_monte_carlo_results
 
-paths = list(data.keys())
-seed_keys = set(path[0] for path in paths)
-sample_keys = sorted(list(set(path[1] for path in paths)))
 
-n_prompts = len(data[paths[0]])
-n_samples = len(sample_keys)
+result_matrix = get_monte_carlo_results()
 
-result_matrix = np.empty((len(seed_keys), n_prompts, len(sample_keys)))
-
-for seed_index, seed_key in enumerate(seed_keys):
-    for sample_index, sample_key in enumerate(sample_keys):
-        row = data[(seed_key, sample_key)]
-        vectorized = np.array([row[prompt_index] for prompt_index in range(n_prompts)])
-        result_matrix[seed_index, :, sample_index] = vectorized
-
-# result_matrix = result_matrix[:, :, :]
+n_prefixes, n_prompts, n_samples = result_matrix.shape
 
 train = result_matrix[:, :, :n_samples // 2]
 valid = result_matrix[:, :, n_samples // 2:]
@@ -29,16 +18,5 @@ valid = result_matrix[:, :, n_samples // 2:]
 train_means = np.mean(train, axis=(1, 2))
 valid_means = np.mean(valid, axis=(1, 2))
 
-# pyplot.plot(train_means, valid_means, "o")
-
-means = np.mean(result_matrix, axis=(1, 2))
-shuffled = np.empty_like(result_matrix)
-for problem in range(result_matrix.shape[1]):
-    for sample in range(result_matrix.shape[2]):
-        shuffled[:, problem, sample] = np.random.permutation(result_matrix[:, problem, sample])
-shuffled_means = np.mean(shuffled, axis=(1, 2))
-
-pyplot.bar(np.arange(len(means)), 100 * np.sort(means), label="Experiment")
-pyplot.bar(np.arange(len(means)), 100 * np.sort(shuffled_means), fc="none", hatch="//", lw=3, ec="orange", label="Control")
-
+pyplot.plot(train_means, valid_means, "o")
 pyplot.show()
